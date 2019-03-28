740af12b596cd331576de8fa64155d2f18a9edac
CopieChloe <chloe.seiler123@live.fr>
2019-03-28 02:45:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+  $( document ).on( "mousemove", function( event ) {
+    $('.starter').remove();
+    let x = event.pageX;
+    let y = event.pageY;
+    let string = 'circle(130px at ' + x + 'px ' + y + 'px)';
+     $('.content').css('clip-path', string);
+  });
+    
+  $(document).click(function(){
+    let x = event.pageX;
+    let y = event.pageY;
+    let stringEnlarge = 'circle(400px at ' + x + 'px ' + y + 'px)';
+    $('.content').css({
+      'clip-path' : stringEnlarge,
+      'transition' : 'all 1s ease'
+   });
+    setInterval(function(){ 
+      $('.content').fadeOut();
+      $('.starter').fadeOut();
+      $('.home').fadeIn();
+      }, 1000);
+    setInterval(function(){ 
+        $('.content').remove();
+        $('.starter').remove();
+      }, 2000);
+ })
+
+
+
+
+
+});
